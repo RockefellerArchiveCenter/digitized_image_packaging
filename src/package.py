719819ca5990d0a7a98f2f0234840bf0efd85bae
Hillel Arnold <hillel.arnold@gmail.com>
2024-03-19 16:06:33 -0400

    refactor for still image

--- src/package.py
+++ src/package.py
@@ -7,7 +7,6 @@ from shutil import copytree, rmtree
 
 import bagit
 import boto3
-import ffmpeg
 from asnake.aspace import ASpace
 from asnake.utils import find_closest_value
 from aws_assume_role_lib import assume_role
@@ -21,8 +20,8 @@ logging.getLogger("bagit").setLevel(logging.ERROR)
 
 class Packager(object):
 
-    def __init__(self, region, role_arn, ssm_parameter_path, refid, rights_ids, tmp_dir, source_dir, destination_bucket,
-                 destination_bucket_video_mezzanine, destination_bucket_video_access, destination_bucket_audio_access, destination_bucket_poster, sns_topic):
+    def __init__(self, region, role_arn, ssm_parameter_path, refid,
+                 rights_ids, tmp_dir, source_dir, destination_bucket, sns_topic):
         self.region = region
         self.role_arn = role_arn
         self.refid = refid
@@ -30,10 +29,6 @@ class Packager(object):
         self.tmp_dir = tmp_dir
         self.source_dir = source_dir
         self.destination_bucket = destination_bucket
-        self.destination_bucket_video_mezzanine = destination_bucket_video_mezzanine
-        self.destination_bucket_video_access = destination_bucket_video_access
-        self.destination_bucket_audio_access = destination_bucket_audio_access
-        self.destination_bucket_poster = destination_bucket_poster
         self.sns_topic = sns_topic
         self.ssm_parameter_path = ssm_parameter_path
         self.service_name = 'digitized_image_packaging'
@@ -55,16 +50,13 @@ class Packager(object):
             ).client
             self.as_repo = config.get('AS_REPO')
             self.move_to_tmp(bag_dir)
-            self.format = self.parse_format(list(bag_dir.glob("*")))
-            self.create_poster(bag_dir)
-            self.deliver_derivatives()
             self.create_bag(bag_dir, self.rights_ids)
             compressed_path = self.compress_bag(bag_dir)
             self.deliver_package(compressed_path)
             self.cleanup_successful_job()
             self.deliver_success_notification()
             logging.info(
-                f'{self.format} package {self.refid} successfully packaged.')
+                f'Package {self.refid} successfully packaged.')
         except Exception as e:
             logging.exception(e)
             self.cleanup_failed_job(bag_dir)
@@ -84,78 +76,6 @@ class Packager(object):
         """
         source_dir = Path(self.source_dir, self.refid)
         copytree(source_dir, dest_dir)
-
-    def parse_format(self, file_list):
-        """Parses format information from file list.
-
-        Args:
-            file_list (list of pathlib.Path instances): List of filepaths in a bag.
-        """
-        if any([f.suffix == '.mp3' for f in file_list]):
-            return 'audio'
-        elif any([f.suffix == '.mp4' for f in file_list]):
-            return 'video'
-        raise Exception(f'Unrecognized package format for files {file_list}.')
-
-    def create_poster(self, bag_dir):
-        """Creates a poster image from a video file.
-
-        Args:
-            bag_dir (pathlib.Path): directory containing local files.
-        """
-        if self.format == 'video':
-            poster = Path(bag_dir, 'poster.png')
-            (
-                ffmpeg
-                .input(Path(bag_dir, f'{self.refid}.mp4'))
-                .filter('thumbnail', 300)
-                .output(str(poster), loglevel="quiet", **{'frames:v': 1})
-                .run()
-            )
-        logging.debug('Poster file {poster} created.')
-
-    def derivative_map(self):
-        """Get information about derivatives to upload to S3.
-
-        Returns:
-            derivative_map (list of three-tuples): path, S3 bucket and mimetype of files.
-        """
-        bag_path = Path(self.tmp_dir, self.refid)
-        if self.format == 'video':
-            return [
-                (bag_path / f"{self.refid}.mov",
-                 self.destination_bucket_video_mezzanine,
-                 "video/quicktime"),
-                (bag_path / f"{self.refid}.mp4",
-                 self.destination_bucket_video_access, "video/mp4"),
-                (bag_path / "poster.png", self.destination_bucket_poster, "image/x-png")
-            ]
-        else:
-            return [
-                (bag_path / f"{self.refid}.mp3",
-                 self.destination_bucket_audio_access, "audio/mpeg"),
-            ]
-
-    def deliver_derivatives(self):
-        """Uploads derivatives to S3 buckets and deletes them from temporary storage."""
-        client = self.get_client_with_role('s3', self.role_arn)
-        transfer_config = boto3.s3.transfer.TransferConfig(
-            multipart_threshold=1024 * 25,
-            max_concurrency=10,
-            multipart_chunksize=1024 * 25,
-            use_threads=True)
-        to_upload = self.derivative_map()
-        for obj_path, bucket, content_type in to_upload:
-            logging.debug(
-                f'Uploading {obj_path} to bucket {bucket} with content type {content_type}')
-            client.upload_file(
-                str(obj_path),
-                bucket,
-                f"{self.refid}{obj_path.suffix}",
-                ExtraArgs={'ContentType': content_type},
-                Config=transfer_config)
-            obj_path.unlink()
-        logging.debug('Derivative files delivered.')
 
     def uri_from_refid(self, refid):
         """Uses the find_by_id endpoint in AS to return the URI of an archival object."""
@@ -233,7 +153,7 @@ class Packager(object):
             'ArchivesSpace-URI': obj_uri,
             'Start-Date': formatted_start_date,
             'End-Date': formatted_end_date,
-            'Origin': 'av_digitization',
+            'Origin': 'digitization',
             'Rights-ID': rights_ids,
             'BagIt-Profile-Identifier': 'zorya_bagit_profile.json'}
         bagit.make_bag(bag_dir, metadata)
@@ -298,12 +218,8 @@ class Packager(object):
         client = self.get_client_with_role('sns', self.role_arn)
         client.publish(
             TopicArn=self.sns_topic,
-            Message=f'{self.format} package {self.refid} successfully packaged',
+            Message=f'Package {self.refid} successfully packaged.',
             MessageAttributes={
-                'format': {
-                    'DataType': 'String',
-                    'StringValue': self.format,
-                },
                 'refid': {
                     'DataType': 'String',
                     'StringValue': self.refid,
@@ -329,12 +245,8 @@ class Packager(object):
         tb = ''.join(traceback.format_exception(exception)[:-1])
         client.publish(
             TopicArn=self.sns_topic,
-            Message=f'{getattr(self, "format", "unknown format")} package {self.refid} failed packaging',
+            Message=f'Package {self.refid} failed packaging.',
             MessageAttributes={
-                'format': {
-                    'DataType': 'String',
-                    'StringValue': getattr(self, 'format', 'unknown format'),
-                },
                 'refid': {
                     'DataType': 'String',
                     'StringValue': self.refid,
@@ -393,13 +305,6 @@ if __name__ == '__main__':
     tmp_dir = os.environ.get('TMP_DIR')
     source_dir = os.environ.get('SOURCE_DIR')
     destination_bucket = os.environ.get('AWS_DESTINATION_BUCKET')
-    destination_bucket_video_mezzanine = os.environ.get(
-        'AWS_DESTINATION_BUCKET_VIDEO_MEZZANINE')
-    destination_bucket_video_access = os.environ.get(
-        'AWS_DESTINATION_BUCKET_VIDEO_ACCESS')
-    destination_bucket_audio_access = os.environ.get(
-        'AWS_DESTINATION_BUCKET_AUDIO_ACCESS')
-    destination_bucket_poster = os.environ.get('AWS_DESTINATION_BUCKET_POSTER')
     sns_topic = os.environ.get('AWS_SNS_TOPIC')
     ssm_parameter_path = f"/{os.environ.get('ENV')}/{os.environ.get('APP_CONFIG_PATH')}"
 
@@ -412,8 +317,4 @@ if __name__ == '__main__':
         tmp_dir,
         source_dir,
         destination_bucket,
-        destination_bucket_video_mezzanine,
-        destination_bucket_video_access,
-        destination_bucket_audio_access,
-        destination_bucket_poster,
         sns_topic).run()
